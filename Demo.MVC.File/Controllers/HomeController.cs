96a7e0e8df008c4f42f8344fc15ecc91fadd0bb3
testNumber1 <accountdemo@gmail.com>
2015-09-01 21:33:15 +0700

    Begin Project
    Project

---
+++ Demo.MVC.File/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo.MVC.File.Models;
+namespace Demo.MVC.File.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult UpFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UpFile(Person model)
+        {
+            HttpPostedFileBase file = HttpContext.Request.Files["file"];
+            string b = "Image/" + file.FileName;
+            string c =Server.MapPath(b);
+            file.SaveAs(c);
+            return View();
+        }
+    }
+}
---
+++ Demo.MVC.File/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.MVC.File.Models
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(20,MinimumLength =6)]
+        public string Name { get; set; }
+
+        public int? Age { get; set; }
+    }
+}
